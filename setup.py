aecfe83e5529b03eda12268691acf61d0f961ad4
KwongTN <5886584+kwongtn@users.noreply.github.com>
2024-07-29 14:50:02 +0800

    Update test suite to use pytest, remove related in setup.py (#771)
    * Fix setuptools to <v72
    
    * Makefile: Use pytest instead of setup.py test
    
    * Attempt to use pytest directly
    
    * Add python -m
    
    * Update setup.py
    
    Co-authored-by: Christian Clauss <cclauss@me.com>
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Add pytest installation to workflow
    
    * Remove pytest from setup.py
    
    * Unlock setuptools
    
    * Remove tests_require
    
    * Move pytest to tox.ini
    
    ---------
    
    Co-authored-by: Christian Clauss <cclauss@me.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    (cherry picked from commit a50011def879c84dd361a85a329dd4f7923d1f0a)

--- setup.py
+++ setup.py
@@ -3,10 +3,8 @@
 import codecs
 import os
 import re
-import sys
 
 import setuptools
-import setuptools.command.test
 
 try:
     import platform
@@ -119,19 +117,6 @@ else:
 
 # -*- %%% -*-
 
-
-class pytest(setuptools.command.test.test):
-    user_options = [('pytest-args=', 'a', 'Arguments to pass to pytest')]
-
-    def initialize_options(self):
-        setuptools.command.test.test.initialize_options(self)
-        self.pytest_args = []
-
-    def run_tests(self):
-        import pytest
-        sys.exit(pytest.main(self.pytest_args))
-
-
 setuptools.setup(
     name=NAME,
     packages=setuptools.find_packages(exclude=[
@@ -148,8 +133,6 @@ setuptools.setup(
     platforms=['any'],
     license='BSD',
     install_requires=reqs('default.txt') + reqs('runtime.txt'),
-    tests_require=reqs('test.txt') + reqs('test-django.txt'),
-    cmdclass={'test': pytest},
     classifiers=classifiers,
     entry_points={
         'celery.beat_schedulers': [
